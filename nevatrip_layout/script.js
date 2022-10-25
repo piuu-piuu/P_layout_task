a801597c12b84e1f27f37e736d2d64b673c94fda
piuu-piuu <91653435+piuu-piuu@users.noreply.github.com>
2022-10-25 21:53:16 +0400

    mobile layout: to do fonts, flag, pic number; to add desktop layout

---
+++ nevatrip_layout/script.js
@@ -0,0 +1,23 @@
+function count_buttons() {
+    let times = document.querySelectorAll('.time-button');
+    let times_count = times.length;
+    if (times_count > 4) {
+        times.forEach((b, i) => {
+            if (i > 2)
+                b.style.display = "none"
+        })
+        times[times_count - 1].innerHTML = "ещё...";
+        times[times_count - 1].style.display = "block";
+    }
+};
+
+function show_buttons() {
+    let buttons = document.querySelectorAll('.time-button');
+    buttons.forEach((b, i) => { b.style.display = "block" })
+    buttons[buttons.length - 1].style.display = "none";
+    // if (x.style.display === "none") {
+    //     x.style.display = "block";
+    // } else {
+    //     x.style.display = "none";
+    // }
+}
